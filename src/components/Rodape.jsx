476d5ea643340d9ca4ad3e45665a083c46b58b10
PaolaFiel <paolacristine2316@gmail.com>
2026-06-14 16:01:05 -0300

    Implementando rodape

---
+++ src/components/Rodape.jsx
@@ -0,0 +1,15 @@
+function Rodape() {
+  return (
+    <footer style={{
+      textAlign: 'center',
+      padding: '15px',
+      color: '#d97706',
+      borderTop: '1px solid #d97706',
+      boxShadow: '0 -5px 15px #d97706'
+    }}>
+      <p>© 2026 Serratec - Grupo 2</p>
+    </footer>
+  );
+}
+
+export default Rodape;
